97cb396558aed6627393de9e83b53ac6ed7b1fad
TrongHoang61tlu <tronghoang19112001@gmail.com>
2022-11-11 00:07:29 +0700

    xây dựng chức năng phân trang trang tìm kiếm

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,22 +3,32 @@ import { useSearchParams } from "react-router-dom";
 import { getProducts } from "../../services/Api";
 
 import ProductItem from "../../shared/components/product-item";
+import Pagination from "../../shared/components/Pagination";
 
 const Search = () => {
-  const [product, setProduc] = useState(null)
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [product, setProduc] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
   const keyword = searchParams.get("keyword");
+  const page = searchParams.get("page") || 1;
 
-  useEffect(()=>{
+  const [pages, setPages] = useState({
+    total: 0,
+    limit: 12,
+    currentPage: page,
+  });
+
+  useEffect(() => {
     getProducts({
-      params :{
-        name : keyword
-      }
-
-    }).then(({data})=>{
-      return setProduc(data.data.docs)
+      params: {
+        name: keyword,
+        limit: 12,
+        page: page,
+      },
+    }).then(({ data }) => {
+      setProduc(data.data.docs);
+      setPages({ ...pages, ...data.data.pages });
     });
-  }, [keyword]);
+  }, [keyword, page]);
   return (
     <>
       <div className="products">
@@ -26,14 +36,13 @@ const Search = () => {
           Kết quả tìm kiếm với sản phẩm <span>{keyword}</span>
         </div>
         <div className="product-list card-deck">
-          {
-            product?.map((product,index)=> {
-              return (
-                <ProductItem key={index} item = {product} />
-              )
-            })
-          }
+          {product?.map((product, index) => {
+            return <ProductItem key={index} item={product} />;
+          })}
         </div>
+      </div>
+      <div id="pagination">
+        <Pagination pages={pages} />
       </div>
     </>
   );
---
+++ src/shared/components/Pagination.js
@@ -0,0 +1,62 @@
+import { useSearchParams, useLocation, Link } from "react-router-dom";
+const Pagination = ({ pages }) => {
+  //  Tính toán các thông số đầu vào cần thiết cho giai thuật phân trang
+  const { pathname, search } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { total, limit, currentPage, hasNext, hasPrev, prev, next } = pages;
+  const totalPages = Math.ceil(total / limit);
+
+  // Tạo ra cấu trúc URL để người dùng có thể click vào trang kết quả cần xem
+  const formatUrl = (page) => {
+    return `${pathname}?keyword=${searchParams.get("keyword")}&page=${page}`;
+  };
+
+  //  Tạo ra một mảng các trang theo cấu trúc 1 ... 8 9 (trang hiện hành) 11 12 ... trang cuối
+  function renderPagesHTML(delta = 2) {
+    const pagesHtml = [];
+    const left = currentPage - delta;
+    const right = currentPage + delta;
+    for (let i = 1; i <= totalPages; i++) {
+      if (
+        i === 1 ||
+        i === currentPage ||
+        i === totalPages ||
+        (i >= left && i <= right)
+      ) {
+        pagesHtml.push(i);
+      }
+    }
+    return pagesHtml;
+  }
+
+  //  Tạo ra danh sách phân trang ở bước trước đó có kết hợp giao diện HTML kèm kiểm tra trạng thái trang đầu, cuối
+  return (
+    <ul className="pagination">
+      {currentPage > 1 ? (
+        <li className="page-item">
+          <Link className="page-link" to={formatUrl(prev)}>
+            Trang trước
+          </Link>
+        </li>
+      ) : null}
+
+      {renderPagesHTML().map((page, index) => (
+        <li className={`page-item ${page === currentPage && "active"}`}>
+          <Link className="page-link" to={formatUrl(page)}>
+            {page}
+          </Link>
+        </li>
+      ))}
+
+      {currentPage < totalPages ? (
+        <li className="page-item">
+          <Link className="page-link" to={formatUrl(next)}>
+            Trang sau
+          </Link>
+        </li>
+      ) : null}
+    </ul>
+  );
+};
+
+export default Pagination;
